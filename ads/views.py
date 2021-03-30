97d9b73f1dc497b2109b6b299a3ac9af7142e203
Nicolas A. Oyarzabal <nicolasoyarzabal@gmail.com>
2021-03-30 23:04:40 +0200

    dj4e-course-3-finished

---
+++ ads/views.py
@@ -0,0 +1,26 @@
+from .models import Ad
+from .owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+
+
+class AdListView(OwnerListView):
+    model = Ad
+    # By convention:
+    # template_name = "ads/ad_list.html"
+
+
+class AdDetailView(OwnerDetailView):
+    model = Ad
+
+
+class AdCreateView(OwnerCreateView):
+    model = Ad
+    fields = ['title', 'text', 'price']
+
+
+class AdUpdateView(OwnerUpdateView):
+    model = Ad
+    fields = ['title', 'text', 'price']
+
+
+class AdDeleteView(OwnerDeleteView):
+    model = Ad
