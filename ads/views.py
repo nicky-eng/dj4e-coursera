ef3bd2448fbf3c2758d156b6c1eca7b90734720f
Nicolas A. Oyarzabal <nicolasoyarzabal@gmail.com>
2021-04-02 22:32:35 +0200

    Ads 2 assignment finished

--- ads/views.py
+++ ads/views.py
@@ -1,12 +1,12 @@
 from django.views import View
 from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Ad
-from .owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
-from .forms import CreateForm
+from .models import Ad, Comment
+from .owner import OwnerListView, OwnerDetailView, OwnerDeleteView
+from .forms import CreateForm, CommentForm
 
 class AdListView(OwnerListView):
     model = Ad
@@ -16,6 +16,13 @@ class AdListView(OwnerListView):
 
 class AdDetailView(OwnerDetailView):
     model = Ad
+    template_name = "ads/ad_detail.html"
+    def get(self, request, pk) :
+        x = Ad.objects.get(id=pk)
+        comments = Comment.objects.filter(ad=x).order_by('-updated_at')
+        comment_form = CommentForm()
+        context = { 'ad' : x, 'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
 
     
 class AdCreateView(LoginRequiredMixin, View):
@@ -76,3 +83,20 @@ def stream_file(request, pk):
     response['Content-Length'] = len(ad.picture)
     response.write(ad.picture)
     return response
+
+class CommentCreateView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        f = get_object_or_404(Ad, id=pk)
+        comment = Comment(text=request.POST['comment'], owner=request.user, ad=f)
+        comment.save()
+        return redirect(reverse('ads:ad_detail', args=[pk]))
+
+
+class CommentDeleteView(OwnerDeleteView):
+    model = Comment
+    template_name = "ads/comment_delete.html"
+
+    # https://stackoverflow.com/questions/26290415/deleteview-with-a-dynamic-success-url-dependent-on-id
+    def get_success_url(self):
+        ad = self.object.ad
+        return reverse('ads:ad_detail', args=[ad.id])
